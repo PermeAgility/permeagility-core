9212351379a16381df3d1d55976465944e7c854f
Glenn Irwin <glenn@irwinideas.com>
2015-09-15 22:00:56 -0600

    Add R Builder - incomplete

---
+++ src/permeagility/plus/r/Data.java
@@ -0,0 +1,92 @@
+/* 
+ * Copyright 2015 PermeAgility Incorporated.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package permeagility.plus.r;
+
+import java.util.HashMap;
+
+import permeagility.util.DatabaseConnection;
+import permeagility.web.Download;
+import permeagility.web.Weblet;
+
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import permeagility.util.QueryResult;
+
+public class Data extends Download {
+
+	@Override
+	public String getContentType() {
+		return "application/json";
+	}
+
+	@Override
+	public String getContentDisposition() {
+		return "inline; filename=\"data.json\"";
+	}
+
+	@Override
+	public byte[] getBytes(DatabaseConnection con, HashMap<String, String> parms) {
+
+		// For view builder sample views.  The other stuff has been implemented elsewhere
+		String view = parms.get("VIEW");
+                String fromTable = parms.get("FROMTABLE");
+                String fromSQL = parms.get("SQL");
+                String callback = parms.get("CALLBACK");
+
+                if (view != null && !view.equals("")) {
+			System.out.println("Build view "+view);
+			ODocument viewDoc = con.get("#"+view);
+			if (viewDoc == null) {
+				return ("Could not retrieve data using "+parms.toString()).getBytes();
+			} else {
+				String sampleData = viewDoc.field("dataScript");
+				return sampleData == null ? "".getBytes() : sampleData.replace("'","\"").getBytes();
+			}
+                        
+		} else if ((fromTable != null && !fromTable.isEmpty()) || (fromSQL != null && !fromSQL.isEmpty())) {
+                    if (callback != null && callback.isEmpty()) { callback = null; }
+                    StringBuilder sb = new StringBuilder();
+
+                    int depth = -1;  // unlimited is the default
+                    try { depth = Integer.parseInt(parms.get("DEPTH")); } catch (Exception e) {}  // It will either parse or not
+
+                    if ((fromTable == null || fromTable.isEmpty()) && (fromSQL == null || fromSQL.isEmpty())) {
+                        return null;
+                    } else {
+                        try {
+                            if (fromSQL == null || fromSQL.isEmpty()) {
+                                fromSQL = "SELECT FROM "+fromTable;
+                            }
+                            QueryResult qr = con.query(fromSQL);
+                            if (callback != null) { sb.append(callback+"("); }
+                            sb.append("{ \""+(fromTable==null ? "data" : fromTable)+"\":[ ");
+                            String comma = "";
+                            for (ODocument d : qr.get()) {
+                                sb.append(comma+permeagility.plus.json.Download.exportDocument(con, d, depth, 0));
+                                comma = ", \n";
+                            }
+                            sb.append(" ] }");
+                            if (callback != null) { sb.append(")"); }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    return sb.toString().getBytes(Weblet.charset);
+                } else {
+                    return "Bad parameters (must specify one of VIEW, FROMTABLE, or SQL), So sorry, no results".getBytes(Weblet.charset);
+                }
+        }	
+
+}
---
+++ src/permeagility/plus/r/PlusSetup.java
@@ -0,0 +1,91 @@
+/* 
+ * Copyright 2015 PermeAgility Incorporated.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package permeagility.plus.r;
+
+import java.util.HashMap;
+
+import permeagility.util.DatabaseConnection;
+import permeagility.util.Setup;
+
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+
+public class PlusSetup extends permeagility.plus.PlusSetup {
+
+	public static boolean INSTALLED = false;  // Set via constant to complete installation
+	public static String INSTALLED_VERSION = "0";  // Set via constant to complete installation
+	
+	// Override these to change the names of the tables that will be created and used by this importer
+	public static String TABLE = "RScript";   // Local OrientDB table name to hold connection specs
+	
+	public static String MENU_CLASS = "permeagility.plus.r.RBuilder";
+	public static String DATA_CLASS = "permeagility.plus.r.Data";
+	
+	public String getName() { return "R Builder"; }
+	public String getInfo() { return "Calculate, twist and plot data using R"; }
+	public String getVersion() { return "0.1.0"; }
+	
+	public boolean isInstalled() { return INSTALLED; }
+	
+	public boolean install(DatabaseConnection con, HashMap<String,String> parms, StringBuilder errors) {
+		OSchema oschema = con.getSchema();
+		String newTableGroup = pickTableGroup(con, parms);
+				
+		if (isNullOrBlank(newTableGroup) || isNullOrBlank(parms.get("MENU")) || isNullOrBlank(parms.get("ROLES"))) {
+			errors.append(paragraph("error","Please specify a table group, menu and the roles to allow access"));
+			return false;
+		}
+
+		OClass table = Setup.checkCreateTable(con, oschema, TABLE, errors, newTableGroup);
+		Setup.checkCreateColumn(con,table, "name", OType.STRING, errors);
+		Setup.checkCreateColumn(con,table, "description", OType.STRING, errors);
+		Setup.checkCreateColumn(con,table, "RScript", OType.STRING, errors);
+		
+		Setup.createMenuItem(con,getName(),getInfo(),MENU_CLASS,parms.get("MENU"),parms.get("ROLES"));	
+		Setup.createMenuItem(con,getName(),getInfo(),DATA_CLASS,null,parms.get("ROLES"));	
+		
+		setPlusInstalled(con, this.getClass().getName(), getInfo(), getVersion());
+		INSTALLED = true;
+		return true;
+	}
+	
+	public boolean remove(DatabaseConnection con, HashMap<String,String> parms, StringBuilder errors) {
+
+		if (parms.get("REMOVE_MENU") != null) {
+			Setup.removeMenuItem(con, MENU_CLASS, errors);
+			Setup.removeMenuItem(con, DATA_CLASS, errors);
+		}
+		
+		String remTab = parms.get("REMOVE_TABLES");
+		if (remTab != null && remTab.equals("on")) {
+			Setup.dropTable(con, TABLE);
+			errors.append(paragraph("success","Table dropped: "+TABLE));
+		}
+
+		setPlusUninstalled(con, this.getClass().getName());
+		INSTALLED = false;
+		return true;
+	}
+	
+	public boolean upgrade(DatabaseConnection con, HashMap<String,String> parms, StringBuilder errors) {
+		// Perform upgrade actions
+				
+		setPlusVersion(con,this.getClass().getName(),getInfo(),getVersion());
+		return true;
+	}
+
+}
