a43fcdf58761a42cf40d8c925431e7797cf5323d
Glenn Irwin <glenn@irwinideas.com>
2015-09-15 23:56:14 -0600

    RBuilder can now start a process - baby steps

--- src/permeagility/plus/r/RBuilder.java
+++ src/permeagility/plus/r/RBuilder.java
@@ -23,9 +23,15 @@ import permeagility.web.Security;
 import permeagility.web.Table;
 
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RBuilder extends Table {
+
+    // Processes in progress by User containing RScript id and process
+    static ConcurrentHashMap<String,ConcurrentHashMap<String,Process>> processes = new ConcurrentHashMap<>();
 
     @Override
     public String getPage(DatabaseConnection con, HashMap<String, String> parms) {
@@ -34,58 +40,73 @@ public class RBuilder extends Table {
         StringBuilder errors = new StringBuilder();
         Locale locale = con.getLocale();
 
-        String submit = parms.get("SUBMIT");
-        String view = parms.get("VIEW");
+        String run = parms.get("RUN");
         String tableName = PlusSetup.TABLE;
-        String editId = parms.get("EDIT_ID");
-        String updateId = parms.get("UPDATE_ID");
-        String additionalScript = "";
-        String additionalStyle = "";
 
         String update = processSubmit(con, parms, tableName, errors);
         if (update != null) {
             return update;
         }
         
-        if (view != null) {
-            System.out.println("Build view " + view);
-            ODocument viewDoc = con.get(view);
-            if (viewDoc == null) {
-                errors.append(paragraph("Could not retrieve view details using " + view));
+        if (run != null) {
+            System.out.println("Build R Process " + run);
+            ODocument runDoc = con.get(run);
+            if (runDoc == null) {
+                errors.append(paragraph("Could not retrieve run details using " + run));
             } else {
-                if (parms.get("DATA") != null) {
-                    String sampleData = viewDoc.field("sampleData");
-                    return sampleData;
+                try {
+                    File t1 = File.createTempFile("Rprocess", "out");
+                    System.out.println("Temp file created "+t1.getAbsolutePath());
+                    String execCommands[] = {"/bin/sh", "-c", "sleep 10; ls > "+t1.getAbsolutePath() };
+                    System.out.println("Running command "+execCommands[2]);
+                    Process newProcess = Runtime.getRuntime().exec(execCommands);
+                    Thread waitForIt = new Thread() {
+                        public void run() {
+                            StringBuilder result = new StringBuilder();
+                            try {
+                                System.out.println("Waiting for process "+newProcess.toString()+" to complete");
+                                int rc = newProcess.waitFor();
+                                System.out.println("R Process waitFor ended with returnCode="+rc);
+                                FileInputStream fis = new FileInputStream(t1);
+                                if (fis.available() > 0) {
+                                    int binc = fis.read();
+                                    do {
+                                        result.append((char)binc);
+                                        binc = fis.read();
+                                    } while (fis.available() > 0);
+                                }
+                            } catch (Exception e) {
+                                System.out.println("Exception in R Process waitfor: "+e.getMessage());
+                                e.printStackTrace();
+                            }
+                            System.out.println("Result is "+result.toString());
+                        }
+                    };
+                    waitForIt.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                parms.put("SERVICE", "Viewing:" + viewDoc.field("name"));
-                sb.append("<button style=\"position: fixed; bottom: 0px;\" id=\"save_as_svg\" download=\"view.svg\">to SVG</button>");
-                additionalStyle = (viewDoc.field("style") != null ? "<style>\n" + viewDoc.field("style") + "</style>\n" : "");
-                additionalScript = (viewDoc.field("pluginURL") != null ? "<script type=\"text/javascript\" src=\"" + viewDoc.field("pluginFiles") + "\"></script>\n" : "");
-                String script = (viewDoc.field("script") != null ? "<script>\n" + viewDoc.field("script") + "</script>\n" : "");
-
-                sb.append(paragraph("" + viewDoc.field("description")));
-                sb.append(form("svgform", hidden("format", "") + hidden("data", "")));
-                sb.append(additionalScript + script);
-            }
+                parms.put("SERVICE", "Running:" + runDoc.field("name"));
+             }
         }
         if (sb.length() == 0) {
             try {
-                parms.put("SERVICE", "View: Setup view");
-                sb.append(paragraph("banner", "Select View"));
-                sb.append(getTable(con, parms, PlusSetup.TABLE, "SELECT FROM " + PlusSetup.TABLE, null, 0, "name, description, button(VIEW:View),-"));
+                parms.put("SERVICE", "R Builder: Setup process");
+                sb.append(paragraph("banner", "Select R Process"));
+                sb.append(getTable(con, parms, PlusSetup.TABLE, "SELECT FROM " + PlusSetup.TABLE, null, 0, "name, description, button_RUN_Run),-"));
             } catch (Exception e) {
                 e.printStackTrace();
-                sb.append("Error retrieving import patterns: " + e.getMessage());
+                sb.append("Error retrieving R Scripts patterns: " + e.getMessage());
             }
         }
-        return head("D3 Builder", getScripts(con) + additionalStyle)
+        return head("R Builder", getScripts(con) )
                 + body(standardLayout(con, parms,
-                    ((Security.getTablePriv(con, PlusSetup.TABLE) & PRIV_CREATE) > 0 && view == null
-                    ? popupForm("CREATE_NEW_ROW", null, Message.get(con.getLocale(), "CREATE_ROW"), null, "NAME",
-                            paragraph("banner", Message.get(con.getLocale(), "CREATE_ROW"))
+                    ((Security.getTablePriv(con, PlusSetup.TABLE) & PRIV_CREATE) > 0 && run == null
+                    ? popupForm("CREATE_NEW_ROW", null, Message.get(locale, "CREATE_ROW"), null, "NAME",
+                            paragraph("banner", Message.get(locale, "CREATE_ROW"))
                             + hidden("TABLENAME", PlusSetup.TABLE)
                             + getTableRowFields(con, PlusSetup.TABLE, parms)
-                            + submitButton(con.getLocale(), "CREATE_ROW"))
+                            + submitButton(locale, "CREATE_ROW"))
                     : "")
                     + errors.toString()
                     + sb.toString()
