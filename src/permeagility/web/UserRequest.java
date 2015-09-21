df8f3e8000bbee2a5f56bccff931de8f9a56e751
Glenn Irwin <glenn@irwinideas.com>
2015-09-21 01:10:28 -0600

    Add messages for UserRequest to support i18n

--- src/permeagility/web/UserRequest.java
+++ src/permeagility/web/UserRequest.java
@@ -16,6 +16,7 @@
 package permeagility.web;
 
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import java.util.Locale;
 import permeagility.util.DatabaseConnection;
 import permeagility.util.Setup;
 
@@ -25,12 +26,13 @@ public class UserRequest extends Table {
     public static String ACCEPT_TO_ROLE = null;
 
      public String getPage(DatabaseConnection con, java.util.HashMap<String,String> parms) {
-	return head(Message.get(con.getLocale(),"REQUEST_ACCOUNT"),getDateControlScript(con.getLocale())+getColorControlScript())+
+	return head(Message.get(con.getLocale(),"REQUEST_LOGIN"),getDateControlScript(con.getLocale())+getColorControlScript())+
 	    body(getHTML(con,parms));
     }
         
     public String getHTML(DatabaseConnection con, java.util.HashMap<String,String> parms) {
     	StringBuilder errors = new StringBuilder();
+        Locale locale = con.getLocale();
     	if (parms.get("SUBMIT") != null) {
             DatabaseConnection serverCon = Server.getServerConnection();
             if (serverCon != null) {
@@ -38,10 +40,10 @@ public class UserRequest extends Table {
                     String name = parms.get(PARM_PREFIX+"name");
                     String pass = parms.get(PARM_PREFIX+"password");
                     if (name == null || name.isEmpty() || pass == null || pass.isEmpty()) {
-                        errors.append(paragraph("error", Message.get(con.getLocale(),"USERREQUEST_NEED_NAMEPASS")));
+                        errors.append(paragraph("error", Message.get(locale,"USERREQUEST_NEED_NAMEPASS")));
                     } else if (serverCon.queryDocument("SELECT FROM userRequest WHERE name='"+name+"'") != null
                             || serverCon.queryDocument("SELECT FROM OUser WHERE name='"+name+"'") != null) {
-                        errors.append(paragraph("error", Message.get(con.getLocale(),"USERREQUEST_EXISTS")));
+                        errors.append(paragraph("error", Message.get(locale,"USERREQUEST_EXISTS")));
                     } else {
                         if (insertRow(con, Setup.TABLE_USERREQUEST, parms, errors)) {
                             if (ACCEPT_TO_ROLE != null && !ACCEPT_TO_ROLE.isEmpty()) {
@@ -51,33 +53,33 @@ public class UserRequest extends Table {
                                     System.out.println("ERROR in UserRequest: ORole "+ACCEPT_TO_ROLE+" not found");
                                     errors.append("Could not find role "+ACCEPT_TO_ROLE);
                                 } else {
-                                    System.out.println("Role "+ACCEPT_TO_ROLE+" found, adding user "+name);
+                                    //System.out.println("Role "+ACCEPT_TO_ROLE+" found, adding user "+name);
                                     // name, password should already be in the request so leave it alone
                                     parms.put(PARM_PREFIX+"status", "ACTIVE");
                                     parms.put(PARM_PREFIX+"roles", roleDoc.getIdentity().toString().substring(1));
                                     if (insertRow(serverCon, "OUser", parms, errors)) {
                                         System.out.println("New user "+parms.get(PARM_PREFIX+"name")+" created");
-                                        return paragraph("success",Message.get(con.getLocale(), "USERREQUEST_CREATED"))+link("/",Message.get(con.getLocale(), "HEADER_LOGO_DESC"));
+                                        return paragraph("success",Message.get(locale, "USERREQUEST_CREATED"))+link("/",Message.get(locale, "HEADER_LOGO_DESC"));
                                     }
                                 }
                             }
                         } else {
-                            return paragraph("success",Message.get(con.getLocale(), "USERREQUEST_INSERTED"))+link("/",Message.get(con.getLocale(), "HEADER_LOGO_DESC"));
+                            return paragraph("success",Message.get(locale, "USERREQUEST_INSERTED"))+link("/",Message.get(locale, "HEADER_LOGO_DESC"));
                         }
                     }
                 } catch (Exception e) {
-                    errors.append(paragraph("error","Could not create user: "+e.getMessage()));
+                    errors.append(paragraph("error",Message.get(locale,"USERREQUEST_ERROR")+e.getMessage()));
                     e.printStackTrace();
                 } finally {
                     Server.freeServerConnection(serverCon);
                 }
             }
     	}
-    	return paragraph("banner",Message.get(con.getLocale(), "USERREQUEST_HEADER"))
+    	return paragraph("banner",Message.get(locale, "REQUEST_LOGIN"))
                +errors
                +form(
                     getTableRowFields(con, Setup.TABLE_USERREQUEST, parms)
-                   +center(submitButton(con.getLocale(), "CREATE_ROW"))
+                   +center(submitButton(locale, "CREATE_ROW"))
                );
     }
                 
