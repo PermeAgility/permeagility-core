c08e080314633516b950fc75837043acd9f5ddf5
Glenn Irwin <glenn@irwinideas.com>
2025-01-16 23:04:53 +0000

    allow for parameters

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ cd target
 java --add-exports java.management/sun.management=ALL-UNNAMED \
  --add-opens java.base/java.util.concurrent.atomic=ALL-UNNAMED \
  --add-opens java.base/java.nio.channels.spi=ALL-UNNAMED \
- -jar permeagility-0.9.0-SNAPSHOT-jar-with-dependencies.jar
+ -jar permeagility-0.9.0-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3 $4
